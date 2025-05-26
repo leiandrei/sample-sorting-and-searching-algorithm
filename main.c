f2fa8f7f199454c2c8d0ab577d1d66f38987a0d1
Lei Andrei Domaoal <leiandreiedomaoal@iskolarngbayan.pup.edu.ph>
2025-05-26 17:38:11 +0800

    Update main.c

--- main.c
+++ main.c
@@ -56,11 +56,26 @@ int *allocated_array(int num)
     return arr;
 }
 
-void swap(int *a, int *b)
+void swap(int *a, int *b) // swapping function
 {
     int temp = *a;
     *a = *b;
     *b = temp;
+}
+
+int partition(int *arr, int low, int high) // partitioning
+{
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
 }
 
 void sort(int *arr, int num)
